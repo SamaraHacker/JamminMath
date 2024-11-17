2f14b90a0275f6ae34c5a01d6825110e4ebad187
SamaraHacker <samara28@iastate.edu>
2024-11-17 11:05:15 -0600

    final updated
    error pushing from other local repo

--- main.py
+++ main.py
@@ -1,2 +1,421 @@
 import pygame
+import turtle as t
+from mathFunctions import basicProblem
+import time
+import webbrowser
 
+
+
+def Unlock(lockedIcon):
+    threshold = 10
+    screen.blit(pygame.transform.scale2x(lockedIcon), (605, 15))
+    screen.blit(pygame.transform.scale2x(lockedIcon), (605, 115))
+    screen.blit(pygame.transform.scale2x(lockedIcon), (605, 215))
+    screen.blit(pygame.transform.scale2x(lockedIcon), (605, 315))
+    screen.blit(pygame.transform.scale2x(lockedIcon), (605, 415))
+    screen.blit(pygame.transform.scale2x(lockedIcon), (605, 515))
+
+    upgradeBtn1.unlocked = True
+    grapeConveyor.unlocked = True
+    if upgradeBtn1.level > threshold:
+        upgradeBtn2.unlocked = True
+        apricotConveyor.unlocked = True
+    if upgradeBtn2.level > threshold:
+       upgradeBtn3.unlocked = True
+       appleConveyor.unlocked = True
+    if upgradeBtn3.level > threshold:
+       upgradeBtn4.unlocked = True
+       blueberryConveyor.unlocked = True
+    if upgradeBtn4.level > threshold:
+       upgradeBtn5.unlocked = True
+       blackberryConveyor.unlocked = True
+    if upgradeBtn5.level > threshold:
+        upgradeBtn6.unlocked = True
+        roseConveyor.unlocked = True
+       
+        
+
+
+class mathWindow:
+    def __init__(self):
+        self.mathWindow = pygame.Rect(0, 0, 600, 600)
+        self.window_font = pygame.font.Font(None, 25)
+        self.answerSubmitted = True
+        self.helpButton = pygame.Rect(490, 30, 100, 50)
+
+        #self.init=basicProblem(mathType, int(level/5))#mathType, difficulty
+        #self.ans = basicProblem.executeProblem(self.init)
+
+
+    def windowPopup(self, mathType, level):#doesnt work
+        self.answerSubmitted= False
+        self.mathType = mathType
+
+        self.init=basicProblem(mathType, int(level/5))#mathType, difficulty
+        self.ans = basicProblem.executeProblem(self.init)
+
+        self.eqn = self.ans[0]#basicProblem(mathType, int(level/5)
+        self.ans = self.ans[1]
+        self.refresh()
+
+    def getHelp(self, mathType):#///////NEED TO GIVE THEM URLS
+        if mathType == 0:#add
+            webbrowser.open("https://www.youtube.com/watch?v=1KxEXKKBndk")
+        elif mathType == 1:#sub
+            webbrowser.open("https://www.youtube.com/watch?v=N2UWPVldcQs")
+        elif mathType == 2:#mult
+            webbrowser.open("https://www.youtube.com/watch?v=RkwoTtljLU4")
+        elif mathType == 3:#div
+            webbrowser.open("https://www.youtube.com/watch?v=6Fy2G4yo2T4")
+        elif mathType == 4:#pow
+            webbrowser.open("https://www.youtube.com/watch?v=uRu3atMemPw")
+        elif mathType == 5:#root
+            webbrowser.open("https://www.youtube.com/watch?v=-VAqyuNW_ss")
+        
+
+    def refresh(self):
+        pygame.draw.rect(screen, "#ffffff", self.mathWindow)
+        pygame.draw.rect(screen, "#0000ff", self.helpButton)
+        self.helpBtnLbl2 = self.window_font.render("Video", True, "#c3c3c3")
+        screen.blit(self.helpBtnLbl2, (515, 47))
+        self.helpBtnLbl = self.window_font.render("Need Help?", True, "#000000")
+        screen.blit(self.helpBtnLbl, (495, 10))
+
+        self.mouse_pos = pygame.mouse.get_pos()
+        if self.helpButton.collidepoint(self.mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.getHelp(self.mathType)
+                time.sleep(1)
+
+
+        self.problemDeclaration = text_font.render(f"Solve: {self.eqn}", True, "#000000")
+        screen.blit(self.problemDeclaration, (200, 125))
+        self.secLine = self.window_font.render("(Round to nearest whole number if applicable)", True, "#000000")
+        if self.mathType == 3:
+            screen.blit(self.secLine, (115, 175))
+        
+    
+
+
+
+
+class BuyButton:
+    def __init__(self, upgradeCost, scoreGiven, yLoc, image, mathType, name):
+        self.yLoc = yLoc
+        self.button = pygame.Rect(685, yLoc+50, 100, 40)
+        self.button_color = "#101010"
+        self.bgColor = "#e3e3e3"
+
+        self.mathType = mathType
+        self.name = name
+
+        self.unlocked = False
+
+        self.clicked = False
+        self.upgrade_description=''
+        self.scoreGiven = scoreGiven
+        self.level = 0
+        self.mps = 0
+        self.buttonFont = pygame.font.Font(None, 25)
+        self.cashFont = pygame.font.Font(None, 20)
+
+        self.display_cost=0
+
+        #self.upgradeBtn = pygame.Rect(10, 50, 175, 75)
+        self.upgrade_cost = upgradeCost
+
+        self.bg = pygame.Rect(600, yLoc, 200, 95)
+
+        self.jamIcon = pygame.image.load(image)
+        
+    def onStart(self):
+        pygame.draw.rect(screen, self.bgColor, self.bg)
+        pygame.draw.rect(screen, self.button_color, self.button)
+    
+    def updateUi(self):
+        self.upgrade_description = self.buttonFont.render(f"+{self.mps}$/sec", True, "#000000")
+        self.display_cost = self.cashFont.render(f"Cost: {str(self.upgrade_cost*(1+self.level))}", True, "#ffffff")
+        self.display_title = self.buttonFont.render(f"{self.name} Jam", True, "#000000")
+        self.display_level = self.buttonFont.render(f"Level {self.level}", True, "#000000")
+        screen.blit(self.display_level, (670, self.yLoc+30))
+        screen.blit(self.display_title, (670, self.yLoc+10))
+        screen.blit(self.upgrade_description, (605, self.yLoc+75))
+        screen.blit(self.display_cost, (690, self.yLoc+60))
+        if self.unlocked:
+            screen.blit(pygame.transform.scale2x(self.jamIcon), (605, self.yLoc+5))
+
+    
+    def click_button(self):
+        self.mouse_pos = pygame.mouse.get_pos()
+        if self.button.collidepoint(self.mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.clicked = True
+            else:
+                if self.clicked:
+                    #enter math popup
+                    if self.unlocked:
+                        if game.money >= self.upgrade_cost*(self.level+1):
+                            mathWindowPopup.windowPopup(self.mathType, self.level)
+
+                    
+                    #if self.unlocked:
+                        #if game.money >= self.upgrade_cost*(self.level+1):
+                            #game.mps += self.scoreGiven
+                            #self.mps += self.scoreGiven
+                            #self.level += 1
+                            #game.money-= self.upgrade_cost*self.level
+                
+                    self.clicked = False
+        
+    def onCheckAnswer(self):
+        #print(mathWindowPopup.ans, " | ", user_text)
+        if str(int(mathWindowPopup.ans)) == user_text:
+            returnStr = "Correct!"
+            rtnColor = "#00c04b"
+            pos = (225, 400)
+            game.mps += self.scoreGiven
+            self.mps += self.scoreGiven
+            self.level += 1
+            game.money-= self.upgrade_cost*self.level
+        else:
+            returnStr = "False"
+            rtnColor = "#e60000"
+            pos = (250, 400)
+            game.money -= self.upgrade_cost*self.level*0.5
+        outputFont = pygame.font.Font(None, 50)
+        outputStr = outputFont.render(returnStr, True, rtnColor)
+        if str(int(mathWindowPopup.ans)) != user_text:
+            correctAnswer = outputFont.render("The answer is: "+str(int(mathWindowPopup.ans)), True, rtnColor)
+        else:
+            correctAnswer = outputFont.render("", True, rtnColor)
+        screen.blit(correctAnswer, (160, 500))
+        screen.blit(outputStr, pos)
+        
+        
+        
+
+
+
+
+
+    def render(self):
+        self.click_button()
+        
+
+
+class Conveyers:
+    def __init__(self, frame1, frame2, x, y):#bool, str, str, int, int
+        self.frame1 = frame1
+        self.frame2 = frame2
+        self.location = (x, y)
+        self.iterator = 0
+        self.speed = 20
+        self.unlocked = 0
+        self.curFrame = "AnimationFrames/EmptyConveyer.png"
+    
+    def anim(self, run):
+        self.iterator+=1
+        if self.unlocked:
+            if not run:
+                self.curFrame="AnimationFrames/EmptyConveyer.png"
+            elif self.iterator%self.speed==0:
+                if self.curFrame == self.frame1:
+                    self.curFrame = self.frame2
+                else:
+                    self.curFrame = self.frame1
+            animFrame = pygame.image.load(self.curFrame)
+            screen.blit(animFrame, (self.location))
+
+        
+
+        
+
+        
+
+class Game:
+    def __init__(self):
+        self.mps = 0
+        self.money = 5
+  
+        self.game_font = pygame.font.Font(None, 15)
+        
+    
+    def increaseMoney(self):
+        
+        self.money+=self.mps/60
+
+
+    def updateConveyers(self):
+        if upgradeBtn1.level == 0:
+            #print(":C")
+            grapeConveyor.anim(False)
+        else:
+            #print("huh")
+            grapeConveyor.anim(True)
+        if upgradeBtn2.unlocked:
+            if upgradeBtn2.level == 0:
+                apricotConveyor.anim(False)
+            else:
+                apricotConveyor.anim(True)
+        if upgradeBtn3.unlocked:
+            if upgradeBtn3.level == 0:
+                appleConveyor.anim(False)
+            else:
+                appleConveyor.anim(True)
+        if upgradeBtn4.unlocked:
+            if upgradeBtn4.level == 0:
+                blueberryConveyor.anim(False)
+            else:
+                blueberryConveyor.anim(True)
+        if upgradeBtn5.unlocked:
+            if upgradeBtn5.level == 0:
+                blackberryConveyor.anim(False)
+            else:
+                blackberryConveyor.anim(True)
+        if upgradeBtn6.unlocked:
+            if upgradeBtn6.level == 0:
+                roseConveyor.anim(False)
+            else:
+                roseConveyor.anim(True)
+        
+
+
+    def draw_score(self):
+        self.display_score = scoreFont.render(f"Money: {str(int(self.money))}", True, "#000000")
+        screen.blit(self.display_score, (25,25))
+
+    
+
+    def render(self):
+        background = pygame.image.load("Background.png")
+        screen.blit(background, (0,0))
+
+        self.updateConveyers() 
+
+        storeIcon = pygame.image.load("Store.png")
+        screen.blit(storeIcon, (50, 50))
+        screen.blit(storeIcon, (50, 350))       
+
+        self.draw_score()
+
+
+
+
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+game = Game()
+
+upgradeBtn1 = BuyButton(5, 2, 10, "grape.png", 0, "Grape")
+upgradeBtn2 = BuyButton(70, 7, 110, "Apricot.png", 1, "Apricot")
+upgradeBtn3 = BuyButton(250, 13, 210, "Apple.png", 2, "Apple")
+upgradeBtn4 = BuyButton(700, 22, 310, "Blueberry.png", 3, "Blueberry")
+upgradeBtn5 = BuyButton(2000, 28, 410, "Blackberry.png", 4, "Blackberry")
+upgradeBtn6 = BuyButton(10000, 37, 510, "Rose.png", 5, "Rose")
+
+grapeConveyor = Conveyers("AnimationFrames/GrapeConveyer1.png", "AnimationFrames/GrapeConveyer2.png", 340, 60)
+apricotConveyor = Conveyers("AnimationFrames/ApricotConveyer1.png", "AnimationFrames/ApricotConveyer2.png", 340, 140)
+appleConveyor = Conveyers("AnimationFrames/AppleConveyer1.png", "AnimationFrames/AppleConveyer2.png", 340, 220)
+blueberryConveyor = Conveyers("AnimationFrames/BlueberryConveyer1.png", "AnimationFrames/BlueberryConveyer2.png", 340, 360)
+blackberryConveyor = Conveyers("AnimationFrames/BlackberryConveyer1.png", "AnimationFrames/BlackberryConveyer2.png", 340, 440)
+roseConveyor = Conveyers("AnimationFrames/RoseConveyer1.png", "AnimationFrames/RoseConveyer2.png", 340, 520)
+
+mathWindowPopup=mathWindow()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Jammin' Math")
+
+
+
+
+text_font = pygame.font.Font(None, 50)
+#title = text_font.render("Jam Game", True, "#000000")
+scoreFont = pygame.font.Font(None, 25)
+
+
+clock = pygame.time.Clock()
+
+
+
+lockedIcon = pygame.image.load("lock.png")
+run = True
+
+
+needToStallToShowResult = False
+user_text = ''
+
+while run:
+
+    if needToStallToShowResult:
+        time.sleep(2)
+        needToStallToShowResult = False
+    if mathWindowPopup.answerSubmitted:
+        game.render()
+        upgradeBtn1.onStart()
+        upgradeBtn2.onStart()
+        upgradeBtn3.onStart()
+        upgradeBtn4.onStart()
+        upgradeBtn5.onStart()
+        upgradeBtn6.onStart()
+        
+        if not upgradeBtn6.unlocked:
+            Unlock(lockedIcon)#not unlocking icons  //////////////////////////////////
+
+        upgradeBtn1.updateUi()
+        upgradeBtn2.updateUi()
+        upgradeBtn3.updateUi()
+        upgradeBtn4.updateUi()
+        upgradeBtn5.updateUi()
+        upgradeBtn6.updateUi()
+        
+        
+    
+    cursor = pygame.mouse.get_pos()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:#remove now
+            if event.key == pygame.K_BACKSPACE:
+                user_text = user_text[:-1]
+                #mathWindowPopup.refresh()
+            elif event.key == pygame.K_RETURN:
+                if user_text != '':
+                    if user_text.isnumeric() or user_text[0] == "-":
+                        mathWindowPopup.answerSubmitted = True
+                        #figure out which button was pressed
+                        buttonList = [upgradeBtn1, upgradeBtn2, upgradeBtn3, upgradeBtn4, upgradeBtn5, upgradeBtn6]
+                        buttonList[mathWindowPopup.mathType].onCheckAnswer()
+                    user_text = ""
+                    needToStallToShowResult = True
+            else:
+                if not mathWindowPopup.answerSubmitted:
+                    user_text += event.unicode
+
+    if not mathWindowPopup.answerSubmitted:
+        input_label = scoreFont.render("Solution:", True, "#000000")
+        mathWindowPopup.refresh()
+    else:
+        input_label = scoreFont.render("", True, "#000000")
+    screen.blit(input_label, (255,300))
+    input_surface = scoreFont.render(user_text, True, "#000000")
+    screen.blit(input_surface, (290-len(user_text)*3,350))
+    
+
+    upgradeBtn1.render()
+    upgradeBtn2.render()
+    upgradeBtn3.render()
+    upgradeBtn4.render()
+    upgradeBtn5.render()
+    upgradeBtn6.render()
+
+    
+
+    clock.tick(60)
+    game.increaseMoney()
+    #screen.blit(title, (50, 50))
+
+    pygame.display.update()
+
+pygame.quit()
---
+++ mathFunctions.py
@@ -0,0 +1,170 @@
+import random
+import math
+
+ADD = 0
+SUB = 1
+MULT = 2
+DIV = 3
+POW = 4
+ROOT = 5
+ALG = 6
+TRIG = 7
+DER = 8
+INT = 9
+
+
+class basicProblem:
+    def __init__(self, mathType, level):
+        self.mathType = mathType
+        self.level = level
+    def executeProblem(self):
+        if self.mathType == ADD:
+            ans = addition(self.level)
+        elif self.mathType == SUB:
+            ans = subtraction(self.level)
+        elif self.mathType == MULT:
+            ans = multiplication(self.level)
+        elif self.mathType == DIV:
+            ans = division(self.level)
+        elif self.mathType == POW:
+            ans = power(self.level)
+        elif self.mathType == ROOT:
+            ans = root(self.level)
+        else:
+            ans = root(self.level)
+        return ans
+
+
+def addition(level):
+    if level == 0:
+        num1 = random.randint(1,10)
+        num2 = random.randint(1,10)
+    elif level == 1:
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+    else:
+        num1 = random.randint(1, 2000)
+        num2 = random.randint(1, 1000)
+    #Output numbers
+    #print(num1, " + ", num2)
+    return (str(num1)+" + "+str(num2)), num1 + num2
+
+def subtraction(level):
+    if level == 0:
+        num2 = random.randint(1,9)   
+        num1 = random.randint(num2,10)
+    elif level == 1:
+        num2 = random.randint(1, 10)
+        num1 = random.randint(1, 100)
+    else:
+        num2 = random.randint(-100, 100)
+        num1 = random.randint(-100, 100)
+    #Output numbers
+    #print(num1, " - ", num2)
+    return (str(num1)+" - "+str(num2)), num1 - num2
+
+def multiplication(level):
+    if level == 0:
+        num1 = random.randint(1,9)
+        num2 = random.randint(1,9)
+    elif level == 1:
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 9)
+    else:
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+    #Output numbers
+    #print(num1, " x ", num2)
+    return (str(num1)+" x "+str(num2)), num1 * num2
+
+def division(level):
+    if level == 0:
+        num2 = random.randint(1,9)
+        num1 = num2*random.randint(1,10)
+    elif level == 1:
+        num1 = random.randint(1, 20) 
+        num2 = random.randint(1, 9)
+    else:
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 10)
+    #Output Numbers
+    #print(num1, " / ", num2)
+    #Decimal point amount
+    return (str(num1)+" / "+str(num2)), int((num1/num2)+0.5)
+
+def power(level):
+    if level == 0:
+        num1 = random.randint(2,20)
+        num2 = 2
+    else:
+        num1 = random.randint(3, 20)
+        num2 = random.randint(2, 3)
+
+    #Output numbers
+    #print(num1, " ^ (", num2, ")")
+    return (str(num1)+" ^ ("+str(num2) + ")"), num1 ** num2
+
+def root(level):
+    if level == 0:
+        root = 2
+        x = random.randint(1,15)**root
+    else:
+        root = 3
+        x = random.randint(1,15)**root
+    #Output numbers
+    #print(x, " ^ (1/", root, ")")
+    return (str(x)+" ^ (1/"+str(root) + ")"), x ** (1/root)
+
+def functionCreate(level):#creates the function string for alg, deriv and integrals
+    functStr=''
+    coeff1 = 0 #First term
+    coeff2 = 0
+    coeff3 = 0
+    power1 = 0
+    power2 = 0
+    const = 0
+    if level == 1:
+        coeff1 = random.randint(2,10)
+        const = random.randint(2, 10)
+        functStr = str(coeff1)+'x +' + str(const)
+    if level == 2:
+        coeff1 = random.randint(2, 10)
+        power1 = 2
+        coeff2 = random.randint(2, 10)
+        const = random.randint(2, 10)
+        functStr = str(coeff1)+'x^' +str(power1) +" + " + str(coeff2) + "x + " + str(const)
+    if level == 3:
+        coeff1 = random.randint(2, 10)
+        power1 = 3
+        coeff2 = random.randint(2, 10)
+        power2 = 2
+        coeff3 = random.randint(2, 10)
+        const = random.randint(2, 10)
+        functStr = str(coeff1) + 'x^ ' + str(power1) + " + " + str(coeff2) + 'x^ ' + str(power2) + " + " + str(coeff3) + "x + " + str(const)
+    return functStr, coeff1, const, coeff2, power1, coeff3, power2
+
+#returns the greatest answer
+def quadraticFormula(a, b, c):
+    x1 = ((-b + math.sqrt(b**2 - (4 * a * c))) / (2 * a))
+    x2 = ((-b - math.sqrt(b**2 - (4 * a * c))) / (2 * a))
+    if x1 >= x2:
+        return x1
+    else:
+        return x2
+
+def algebra(level):
+    answer = 0
+    if level == 0:
+        y = random.randint(1,30)
+        funct = functionCreate(1)
+        coeff = funct[1]
+        const = funct[2]
+        answer = round(((y - const) / coeff), 1)
+    else:
+        y = 0
+        funct = functionCreate(2)
+        coeff1 = funct[1]
+        const = funct[2]
+        coeff2 = funct[3]
+        answer = quadraticFormula(coeff1, coeff2, const)
+    return (str(y) + " = " + funct[0]), answer
